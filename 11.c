1e391e2c52c8b036602437e650656a35be53b81e
Christo Thomas <104912430+Rc291@users.noreply.github.com>
2024-07-17 15:47:33 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+int x=10;y=20;z;
+int main()
+{
+z=(x*x*x)<(y*x*x);
+printf("%d",z);
+}
---
+++ 9.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+int x=10;y=20;z;
+int main()
+{
+z=x<=y;
+printf("%d",z);
+}
